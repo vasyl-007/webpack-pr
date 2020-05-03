bddcadbdea6713ee7b6f17550b719fc981439efe
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-03 22:56:27 +0300

    webpack demo with connected styles, css loaders, html plugin

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import message from "./message";
+import arrow from "./arrow";
+import "./styles.css";
+
+console.log(message);
+console.log(arrow(5, 10));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+console.log("DIRNAME", __dirname);
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
