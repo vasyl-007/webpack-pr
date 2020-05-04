e8c985b6e0de602c697db30727c79def431b5f4b
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-04 19:19:27 +0300

    webpack base fina

--- src/arrow.js
+++
@@ -1,2 +0,0 @@
-const arrow = (a, b) => a + b + 50;
-export default arrow;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import message from "./message";
-import arrow from "./arrow";
-import "./styles.css";
-import listItemTemplate from './list-item.hbs'
-
-console.log(listItemTemplate);
-console.log(message);
-console.log(arrow(5, 10));
+import './styles.css';
